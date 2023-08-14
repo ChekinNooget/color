b1345f4b2d15bfa0b27c617f53553167369851a3
ChekinNooget <85902233+ChekinNooget@users.noreply.github.com>
2023-08-13 20:56:32 -0600

    Update color-picker.js

--- color-picker.js
+++ color-picker.js
@@ -1,3 +1,7 @@
+function setColorsDefault(){
+	localStorage.setItem("color", "#c2c0b3 #000000 #9e9e96 #61626b #975e45 #433233 #f28aa8 #cd4354 #b8221b #7fc4cb #174487 #0f1a74 #dbc38a #eeb82b #fbb124 #fe8575 #f04b1a #d968a5 #7fc45a #57b79c #1f6b37 #3a5f4d #a288ac #4d4382 ")	
+}
+
 function addColor(color) {
 	var pickers = document.getElementById("color-pickers")
 	var pickerCount = document.getElementsByClassName("picker-group").length
